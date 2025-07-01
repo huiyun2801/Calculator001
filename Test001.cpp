33c6ff430d2963082c2f706ba8b92043aaa3969b
huiyun2801 <huiyun2103@gmail.com>
2025-07-02 02:58:14 +0800

    Update Test001.cpp

--- Test001.cpp
+++ Test001.cpp
@@ -8,6 +8,28 @@ struct Node {
 
 int main(){
   Node* head, * tail, * mover;
+  int i;
+
+  head=tail=NULL;
+
+  for(i=1;i<=15;i++){
+    Node* N = new Node {i,NULL};
+
+    if(head=NULL) {
+      head=N;      
+    }
+    else {
+      mover=tail;
+      mover->next=N;      
+    }
+    tail=N;    
+  }
+
+  mover=head;
+  while (mover->next!=NULL){
+    cout<<mover->num<<"]";
+    mover=mover->next;
+  }
   
   
   return 0;
