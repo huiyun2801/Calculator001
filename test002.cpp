7eda21726e3ea29662a4c7487e23779f407fbb3f
huiyun2801 <huiyun2103@gmail.com>
2025-07-29 02:02:26 +0800

    Update test002.cpp

--- test002.cpp
+++ test002.cpp
@@ -2,3 +2,16 @@
 #include <string>
 
 using namespace std;
+
+struct Node {
+  int num;
+  Node* next; 
+};
+
+void BuildList(Node* &head, Node* &tail) {
+  int i;
+  Node* move;
+
+  head = tail = move = NULL;
+  
+}
