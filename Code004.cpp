dd02926980036eca29a355f1d35f615c424179dc
huiyun2801 <huiyun2103@gmail.com>
2025-08-03 02:19:49 +0800

    Update Code004.cpp

--- Code004.cpp
+++ Code004.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct Cal {
@@ -6,3 +7,10 @@ struct Cal {
   float num;
   Cal* next;
 };
+
+void KeyBoard(string){
+  cout << "Enter : ";
+  cin >> 
+}
+
+void BuildList
