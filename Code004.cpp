641b17afaa3e8e2fc59f771716950e140337f717
huiyun2801 <huiyun2103@gmail.com>
2025-08-03 03:13:11 +0800

    Update Code004.cpp

--- Code004.cpp
+++ Code004.cpp
@@ -8,9 +8,65 @@ struct Cal {
   Cal* next;
 };
 
-void KeyBoard(string){
+void KeyBoard(string &input){
   cout << "Enter : ";
-  cin >> 
+  cin >> input;
 }
 
-void BuildList
+void BuildList (string input, Cal* &head, Cal* &tail, bool &valid){
+  int i, len;
+  char operation = '+';
+  float number = 0;
+  Cal* move;
+
+  len = input.length();
+  head = tail = NULL;
+
+  for (i=0, valid=true; valid && i<=len; i++){
+    if (input[i]>47 && input[i]<58) {number = number * 10 + input[i];}
+    
+    else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i] == 0) {
+      Cal* c = new Cal {operation, number, NULL};
+
+      if(head==NULL) {head = c;}
+      else {tail.next = c;}
+      
+      tail = c;
+    }
+
+    else {
+			cout << "Invalid character detected!";
+			valid = false;
+		}
+  }
+  
+}
+
+void PrintList(Cal* head, bool valid, float result) {
+
+	Cal* move = head;
+
+	while (valid && move != NULL) {
+		cout << "[" << move->op << "] " << move->num << endl;
+		move = move->next;
+	}
+
+	if (valid) { cout << "[=]" << result; }
+	else { cout << "Infinity :("; }
+
+}
+
+int main() {
+
+	string input;
+	Cal* head, * tail;
+	bool valid = true;
+	float result = 0;
+
+	KeyBoard(input);
+	BuildList(input, head, tail, valid);
+	
+	PrintList(head, valid, result);
+
+	return 0;
+}
